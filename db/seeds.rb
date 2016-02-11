e82f2095b550ba717728f6c9c88f837ddcc9a71f
Eric Kramer <cosmocracy@yahoo.com>
2016-02-11 16:22:36 -0500

    Installed ActiveAdmin gem Generated migrations for basic Site/SiteType/Institution tables Created seed file for site types Configured ActiveAdmin to allow CRUD on site_types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+SiteType.create( name: 'Collection Site', description: 'Location where biospecimens are collected')
+SiteType.create( name: 'Laboratory', description: 'Location where biospecimens are processed')
+SiteType.create( name: 'Repository', description: 'Location where biospecimens are stored')
+SiteType.create( name: 'Room', description: 'Location where biospecimens are stored')
+SiteType.create( name: 'Pathologist', description: 'Location where biospecimens are Reviewed')
+SiteType.create( name: 'not specified', description: 'Location is not specified')
+SiteType.create( name: 'Path Center', description: 'BPC Path')
